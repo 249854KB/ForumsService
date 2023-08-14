3c21f7828ec9de5ceb0bad5ed05fd035437848df
printfKrzysztof <krzysztofbortnowski@outlook.com>
2023-08-14 11:40:48 +0200

    Changing structure to sub_repos

---
+++ Dtos/UserReadDto.cs
@@ -0,0 +1,10 @@
+namespace ForumsService.Dtos
+{
+    public class UserReadDto
+    {
+        public int Id { get; set;}
+        public string Name { get; set;}
+         public int RankInSystem { get; set; }
+        public int NumberOfDogs { get; set; }
+    }
+}
